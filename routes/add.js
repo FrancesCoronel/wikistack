82d66c29a5a116ac6d7954ea105c1b807b96f5b6
Jovia Nierenberg <nierenberg.jovia@gmail.com>
2015-06-24 14:55:48 -0400

    Added url_name to add.js

--- routes/add.js
+++ routes/add.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 // add
 router.get('/add/', function(req, res, next){
 	res.render('addpage');
-})
+});
 
 // submit
 router.post('/submit', function(req, res) {
@@ -22,11 +22,25 @@ router.post('/submit', function(req, res) {
   console.log("title: ", title);
   var content = req.body.content;
   console.log("content: ", content);
+  var url_name = generateUrlName(title);
+  console.log("url: ", url_name);
+
   
 
-  var page = new models.Page({ 'title': title, 'content': content });
+  var page = new models.Page({ 'title': title, 'content': content, 'url_name': url_name });
   page.save();
   res.redirect('/');
 });
+
+var generateUrlName = function(name) {
+  if (typeof name != "undefined" && name !== "") {
+    // Removes all non-alphanumeric characters from name
+    // And make spaces underscore
+    return name.replace(/\s/ig, '_').replace(/\W/ig,'');
+  } else {
+    // Generates random 5 letter string
+    return Math.random().toString(36).substring(2,7);
+  }
+};
 
 module.exports = router;
